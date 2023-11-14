d20e44481a8db051bdc185e45f70984a39d3e583
aam-himel <aam.himel@gmail.com>
2023-11-14 13:52:45 +0600

    array to linkedlist cpp

--- linked-list/generate-a-linked-list-from-given-array.cpp
+++ linked-list/generate-a-linked-list-from-given-array.cpp
@@ -3,24 +3,58 @@ using namespace std;
 
 class node
 {
+public:
     int data;
     node *next;
 
     node(int val)
     {
-        next->data = val;
-        next->next = NULL;
+        data = val;
+        next = NULL;
     }
 };
 
-node *arrayToList(node *&head, vector<int> arr)
+// array to linked list conversion
+void arrayToLinkdedList(vector<int> nums, node *&head)
 {
+    node *temp = head;
+    if (nums.size() == 0)
+    {
+        return;
+    }
+    if (nums.size() == 1)
+    {
+        head = new node(nums[0]);
+        temp = head;
+        return;
+    }
+    head = new node(nums[0]);
+    temp = head;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        node *item = new node(nums[i]);
+        temp->next = item;
+        temp = temp->next;
+    }
+}
+
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "  ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
 int main()
 {
-    node *head = new node(10);
-    cout << head;
-
+    node *head = NULL;
+    vector<int> nums = {1, 2, 3};
+    arrayToLinkdedList(nums, head);
+    display(head);
     return 0;
 }
